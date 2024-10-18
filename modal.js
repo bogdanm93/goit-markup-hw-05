93db1c9f11b8fcce6575ed2987d41b1b64d9ddb3
BOgdan <munteanu.bbogdan@gmail.com>
2024-10-18 13:39:45 +0300

    first commit

--- modal.js
+++ modal.js
@@ -3,12 +3,19 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
+      form: document.querySelector(".modal-form")
     };
-  
+
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
-  
+
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
     }
-  })();
+
+    // Prevent form submission and close modal
+    refs.form.addEventListener("submit", (event) => {
+        event.preventDefault(); // Prevent the default form submission
+        toggleModal(); // Close the modal
+    });
+})();
